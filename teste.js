682b144fa83947f221b6973035cf7042dd578403
Raphael Feijó <824135407@ulife.com.br>
2025-09-02 11:38:19 -0300

    aaaa

---
+++ teste.js
@@ -0,0 +1,11 @@
+let num1=10;    
+let num2=20;
+ 
+let soma = num1 + num2;
+console.log("A soma é: " + soma);
+
+if (soma > 20) {
+    console.log("A soma é maior que 20");
+} else {
+    console.log("A soma é menor ou igual a 20");
+}
