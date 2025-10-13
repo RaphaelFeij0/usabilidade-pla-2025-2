7187686ec5220b789fb591e7b079a9dfdd0664c6
Raphael Feijó <824135407@ulife.com.br>
2025-10-13 11:05:26 -0300

    chore: update css

--- meu-primeiro-app-react/src/index.js
+++ meu-primeiro-app-react/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
+import './style.css';
 
 const App = () => {
+const estilosBotão = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'rgb(95,164,48)', color: 'white', border: 'none', width: '100%', borderRadius: 8}
+const textoRotulo = "Nome: ";
+
   return  <div style= {{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 17, borderRadius: 8}}>
-        <label for="nome" style= {{display: 'block', marginBottom: 4}}>Nome: </label>
+        <label className="rotulo" htmlFor="nome" style= {{display: 'block', marginBottom: 4}}>{textoRotulo} </label>
         <input type="text" id="nome" style= {{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none', boxSizing: 'border-box'}}/>
-        <button style= {{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'rgb(248, 156, 51)', color: 'white', border: 'none', width: '100%', borderRadius: 8}}>Enviar</button>
+        <button style={estilosBotão}>Enviar</button>
     </div>
   
 }
